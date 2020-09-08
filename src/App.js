0852f000e3cbc11c941afb4cbbab9a759ab8f2ab
Parth Dubal <dubalparth@gmail.com>
2020-09-08 15:45:07 -0400

    Header is added with CSS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     // BEM
     <div className="app">
-      <h1> Hello Clever Programmers, let's build the Amazon Store </h1>
+      <Header />
+      {/* Home */}
     </div>
   );
 }

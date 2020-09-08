d48aa11d19ed4f170d28f58f51090a3e071b2b3d
Parth Dubal <dubalparth@gmail.com>
2020-09-08 14:54:10 -0400

    Firebase config added

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBDMiUaFfPB4y8q9DF4XYZYXsh3dfg6c-E",
+    authDomain: "clone-c18a0.firebaseapp.com",
+    databaseURL: "https://clone-c18a0.firebaseio.com",
+    projectId: "clone-c18a0",
+    storageBucket: "clone-c18a0.appspot.com",
+    messagingSenderId: "1054108466827",
+    appId: "1:1054108466827:web:4cd47700ccbe192eee1e8e"
+};
